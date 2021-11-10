53322e96e3dbe4e6604541452144b54efd6c3dd9
mikamada <mikamada17@gmail.com>
2021-11-10 23:27:49 +0700

    membuat file theme.dart dan menambahkan variable color, textStyle, fontWeight

---
+++ lib/shared/theme.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color promaryColor = Color(0xff4C74DA);
+Color backgroundColor = Color(0xffFEEFF2);
+Color blackColor = Color(0xff000000);
+Color whiteColor = Color(0xffffffff);
+Color greyColor = Color(0xffAFAFAF);
+Color darkGreyColor = Color(0xffB9B9B9);
+Color brownColor = Color(0xffE14545);
+Color greenColor = Color(0xffFFB341);
+
+TextStyle blackTextStyle = GoogleFonts.poppins(
+  color: blackColor,
+);
+
+TextStyle whiteTextStyle = GoogleFonts.poppins(
+  color: whiteColor,
+);
+
+TextStyle greyTextStyle = GoogleFonts.poppins(
+  color: greyColor,
+);
+
+TextStyle brownTextStyle = GoogleFonts.poppins(
+  color: brownColor,
+);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight extraBold = FontWeight.w800;
+FontWeight black = FontWeight.w900;
