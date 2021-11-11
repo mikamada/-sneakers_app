b0f2319d56e1800db7dbb4173be0726418005deb
mikamada <mikamada17@gmail.com>
2021-11-11 10:19:58 +0700

    mmebuat file sign_up.dart

---
+++ lib/ui/pages/sign_up.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:sneakers_app/shared/theme.dart';
+
+class SignUp extends StatelessWidget {
+  const SignUp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: backgroundColor,
+    );
+  }
+}
