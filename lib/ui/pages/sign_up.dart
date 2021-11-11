0e37a57c968806027ee300cdab995fc005c90f15
mikamada <mikamada17@gmail.com>
2021-11-11 11:01:35 +0700

    membuat text form field pada sign up page

--- lib/ui/pages/sign_up.dart
+++ lib/ui/pages/sign_up.dart
@@ -6,16 +6,70 @@ class SignUp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Widget title() {
+      return Center(
+        child: Text(
+          'Join us and get\nYour dream sneakers',
+          style: blackTextStyle.copyWith(
+            fontSize: 24,
+            fontWeight: semiBold,
+          ),
+          textAlign: TextAlign.center,
+        ),
+      );
+    }
+
+    Widget textFieldForm() {
+      return Container(
+        margin: EdgeInsets.only(top: 40),
+        padding: EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 30,
+        ),
+        decoration: BoxDecoration(
+          color: whiteColor,
+          borderRadius: BorderRadius.circular(17),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Full name',
+              style: blackTextStyle.copyWith(fontSize: 18, fontWeight: medium),
+            ),
+            SizedBox(
+              height: 6,
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                prefixIcon: Icon(
+                  Icons.account_circle,
+                  color: primaryColor,
+                ),
+                hintText: 'Your full name',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(17),
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
     return Scaffold(
       backgroundColor: backgroundColor,
       body: SafeArea(
         child: ListView(
-          children: [
-            Center(
-              child: Text('sign Up Page'),
+            padding: EdgeInsets.only(
+              left: 24,
+              right: 24,
+              top: 50,
             ),
-          ],
-        ),
+            children: [
+              title(),
+              textFieldForm(),
+            ]),
       ),
     );
   }
